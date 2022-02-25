6393c945e979a20cdf69632b4278804a7e3af0ce
michael-cwm <michaelcwm@hotmail.com>
2022-02-25 13:38:58 +0100

    updated landing page

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 require("dotenv").config();
+require("./mongoose");
+
 const express = require("express");
 const exphbs = require("express-handlebars");
+
+const usersRouter = require("./routes/users-router.js");
 
 const app = express();
 app.use(express.json());
@@ -8,19 +12,21 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.engine(
-  "hbs",
-  exphbs.engine({
-    defaultLayout: "main",
-    extname: ".hbs",
-  })
+    "hbs",
+    exphbs.engine({
+        defaultLayout: "main",
+        extname: ".hbs",
+    })
 );
 
 app.set("view engine", "hbs");
 
 app.get("/", async (req, res) => {
-  res.render("home");
+    res.render("home");
 });
 
+app.use("/users", usersRouter);
+
 app.listen(8000, () => {
-  console.log("http://localhost:8000");
+    console.log("http://localhost:8000");
 });
