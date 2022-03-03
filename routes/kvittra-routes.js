40ec7915fa0bb9845ceaa18ec86de7080891c016
RasWeb <rasmuspalm@fastmail.se>
2022-03-03 13:00:19 +0100

    removed comments

--- routes/kvittra-routes.js
+++ routes/kvittra-routes.js
@@ -4,40 +4,6 @@ const router = express.Router();
 const jwt = require("jsonwebtoken");
 const { ObjectId } = require("mongodb");
 
-// FOR ERRORS
-// let id = undefined;
-// try {
-//   id = req.params.id;
-// } catch {
-//   next();
-// }
-// const { token } = req.cookies;
-// if (token && jwt.verify(token, process.env.JWTSECRET)) {
-//   if (id) {
-//     // logged in
-//     res.render("");
-//   } else {
-//     res.redirect("/unauthorized");
-//   }
-// }
-
-// let id = undefined;
-
-// try {
-//   id = req.params.id;
-// } catch {
-//   next();
-// }
-
-// const { token } = req.cookies;
-// if (token && jwt.verify(toke, process.env.JWTSECRET)) {
-//   if (id) {
-//     res.render("");
-//   }
-// } else {
-//   res.redirect("/unauthorized");
-// }
-
 router.post("/", async (req, res) => {
   const { token } = req.cookies;
   const tokenData = jwt.decode(token, process.env.JWTSECRET);
