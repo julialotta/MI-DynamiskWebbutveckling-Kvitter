6f809a743a10adf7a2d869adde7caf90f35ccfc6
julialotta <julialottatinglof@gmail.com>
2022-03-08 14:47:06 +0100

    moved forgot you password

--- routes/likes-routes.js
+++ routes/likes-routes.js
@@ -4,7 +4,6 @@ const express = require("express");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
 const { ObjectId } = require("mongodb");
-const utils = require("../utils");
 
 // ID FUNCTION \\
 function getId(id, next) {
@@ -24,18 +23,26 @@ router.get("/:id", async (req, res) => {
   const { token } = req.cookies;
   const tokenData = jwt.decode(token, process.env.JWTSECRET);
 
-  const newLike = new LikesModel({
-    post: req.params.id,
-    likedBy: tokenData.userId,
+  const postId = ObjectId(req.params.id);
+
+  LikesModel.findOne(postId, async (err, post) => {
+    if (post) {
+      // pusha in ny like i likedBy-array
+    } else {
+      const newLike = new LikesModel({
+        post: req.params.id,
+        likedBy: tokenData.userId,
+      });
+      if (newLike) {
+        await newLike.save();
+        res.redirect("/");
+      } else {
+        res.render("home", {
+          error: "something went wrong",
+        });
+      }
+    }
   });
-  if (newLike) {
-    await newLike.save();
-    res.redirect("/");
-  } else {
-    res.render("home", {
-      error: "something went wrong",
-    });
-  }
 });
 
 module.exports = router;
